b7b934dce65ce121467bf73e14dcda21f754710e
Sara <sarabravo@2000gmail.com>
2024-10-21 23:42:21 -0300

    clase For

---
+++ clase6FOr/ContadorPares.java
@@ -0,0 +1,7 @@
+package clase6FOr;
+/*Escribe un programa que genere un array de 10 elementos de tipo entero. Inicializa cada elemento con un número aleatorio menor a 100 y luego imprime todos los elementos del array en una única línea. Posteriormente, recorre el array para contar los elementos pares y muestra el total en la consola. */
+public class ContadorPares {
+    public static void main(String[] args) {
+        
+    }
+}
